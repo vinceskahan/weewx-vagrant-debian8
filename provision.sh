aa6d8888eb8ebe9fd55402a63723ed8c481c1e96
vince <vince>
2015-08-15 13:26:51 -0700

    get latest release or git clone based on variable in provision.sh

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 ########################################################################
 #
-# quick provisioning script for weewx-3.1.0 on debian-8 under vagrant
-#    2015-0522 - vinceskahan@gmail.com - original
+# quick provisioning script for weewx-3.2.1 on debian-8 under vagrant
+#    2015-0814 - vinceskahan@gmail.com - weewx 3.2.1 with US debian mirrors
 #
 ########################################################################
 
@@ -10,7 +10,9 @@ cp /vagrant/sources.list /etc/apt/sources.list
 
 # patch up to current
 sudo apt-get update
-sudo apt-get upgrade
+
+# this slows my dev cycle down too much
+# sudo apt-get upgrade
 
 # get ancillary stuff not in the base box
 sudo apt-get install -y sqlite3 lynx wget curl procps nginx
@@ -18,24 +20,40 @@ sudo apt-get install -y sqlite3 lynx wget curl procps nginx
 # get weewx prerequisites
 sudo apt-get install -y python-configobj python-cheetah python-imaging python-serial python-usb python-dev
 
-# download/build/install weewx in simulator mode
-wget http://www.weewx.com/downloads/weewx-3.2.1.tar.gz -O /tmp/weewx.tgz
-cd /tmp
-echo "...extracting weewx..."
-tar zxf /tmp/weewx.tgz
-echo "...building weewx..."
-cd weewx-* ; ./setup.py build ; sudo ./setup.py install --no-prompt
+#-------------------------------
+# uncomment your desired method to download weewx sources
 
-# link it into the web (debian) - the path is different on ubuntu
+# DOWNLOAD_METHOD="git"             # bleeding edge
+DOWNLOAD_METHOD="prepackaged"       # official version
+
+#-------------------------------
+
+echo "...downloading weewx..."
+if [ "x${DOWNLOAD_METHOD}" = "xgit" ]
+then
+   sudo apt-get install -y git
+   git clone https://github.com/weewx/weewx.git /tmp/weewx-current
+else
+   # this assumes Tom always has his tarball with a top directory weewx-x.y.z
+   wget http://www.weewx.com/downloads/weewx-3.2.1.tar.gz -O /tmp/weewx.tgz
+   echo "...extracting weewx..."
+   tar zxf /tmp/weewx.tgz
+fi
+
+echo "...building weewx (simulator mode)..."
+cd /tmp/weewx-* ; ./setup.py build ; sudo ./setup.py install --no-prompt
+
+# link it into the web (debian)
+# IMPORTANT: the path might be different on ubuntu
 sudo  ln -s /var/www/html /home/weewx/public_html
 
-# put upstream config file into place
+# put system startup file into place
 sudo cp /home/weewx/util/systemd/weewx.service /etc/systemd/system
 
-# then fix 3.1.0 by copying in a version that works on debian-8.0
-sudo cp /vagrant/weewx.service /etc/systemd/system
+# patching/fixing is not needed as of 3.2.0
+# sudo cp /vagrant/weewx.service /etc/systemd/system
 
-# similarly put 'our' weewx.conf into place (enables debug=1)
+# put 'our' weewx.conf into place (enables debug=1)
 sudo cp /vagrant/weewx.conf /home/weewx/weewx.conf
 
 # enable the service
